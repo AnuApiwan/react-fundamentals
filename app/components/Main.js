6c556384d0fda17b59e92492dab97c9a47c196e7
Anu Apiwan <anu@innovisor.co.th>
2017-06-01 16:14:43 +0700

    Complete Lesson 5

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,7 +5,7 @@ var Main = React.createClass({
         return (
             <div>
                 This is main!
-        {this.props.children}
+                {this.props.children}
             </div>
         )
     }
--- app/config/routes.js
+++ app/config/routes.js
@@ -3,16 +3,19 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var hashHistory = ReactRouter.hashHistory;
+// var hashHistory = ReactRouter.hashHistory;
+var browserHistory = ReactRouter.browserHistory;
+var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
-var Home = require('../components/Home');
+var Home = require("../components/Home");
 
 var routes = (
-    <Router history={hashHistory} >
+    <Router history={browserHistory}>
         <Route path='/' component={Main}>
-            <Route path='/home' component={Home} />
+            <IndexRoute component={Home} />
         </Route>
-    </Router >
+    </Router>
 );
 
 module.exports = routes;
+
